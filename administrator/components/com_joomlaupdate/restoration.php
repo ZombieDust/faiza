8fc9411fb5091d5f42339c75d8dcef1d7355dc06
Павел <krivyshiN@bk.ru>
2015-05-14 14:06:49 +0700

    Добавлен архив сайта,  #1

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'FPT9syL4AM2chzw4Zhfc2kMXmBd8hL5U',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => '/home/h349271/deeboro.ru/docs/tmp/Joomla_3.4.0-Stable-Update_Package.zip',
+	'kickstart.setup.destdir' => '/home/h349271/deeboro.ru/docs',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
---
+++ templates/beez3/index.php
@@ -0,0 +1,252 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.beez3
+ * 
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+$this->setGenerator('Рок-мастерская Dee Boro | Продажа рок-атрибутики | deeboro.ru');
+JLoader::import('joomla.filesystem.file');
+
+// Check modules
+$showRightColumn = ($this->countModules('position-3') or $this->countModules('position-6') or $this->countModules('position-8'));
+$showbottom      = ($this->countModules('position-9') or $this->countModules('position-10') or $this->countModules('position-11'));
+$showleft        = ($this->countModules('position-4') or $this->countModules('position-7') or $this->countModules('position-5'));
+
+if ($showRightColumn == 0 and $showleft == 0)
+{
+	$showno = 0;
+}
+
+JHtml::_('behavior.framework', true);
+
+// Get params
+$color          = $this->params->get('templatecolor');
+$logo           = $this->params->get('logo');
+$navposition    = $this->params->get('navposition');
+$headerImage    = $this->params->get('headerImage');
+$doc            = JFactory::getDocument();
+$app            = JFactory::getApplication();
+$templateparams	= $app->getTemplate(true)->params;
+$config         = JFactory::getConfig();
+$bootstrap      = explode(',', $templateparams->get('bootstrap'));
+$jinput         = JFactory::getApplication()->input;
+$option         = $jinput->get('option', '', 'cmd');
+
+if (in_array($option, $bootstrap))
+{
+	// Load optional rtl Bootstrap css and Bootstrap bugfixes
+	JHtml::_('bootstrap.loadCss', true, $this->direction);
+}
+
+$doc->addStyleSheet(JUri::base() . 'templates/system/css/system.css');
+$doc->addStyleSheet(JUri::base() . 'templates/' . $this->template . '/css/position.css', $type = 'text/css', $media = 'screen,projection');
+$doc->addStyleSheet(JUri::base() . 'templates/' . $this->template . '/css/layout.css', $type = 'text/css', $media = 'screen,projection');
+$doc->addStyleSheet(JUri::base() . 'templates/' . $this->template . '/css/print.css', $type = 'text/css', $media = 'print');
+$doc->addStyleSheet(JUri::base() . 'templates/' . $this->template . '/css/general.css', $type = 'text/css', $media = 'screen,projection');
+$doc->addStyleSheet(JUri::base() . 'templates/' . $this->template . '/css/' . htmlspecialchars($color) . '.css', $type = 'text/css', $media = 'screen,projection');
+
+if ($this->direction == 'rtl')
+{
+	$doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/template_rtl.css');
+	if (file_exists(JPATH_SITE . '/templates/' . $this->template . '/css/' . $color . '_rtl.css'))
+	{
+		$doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/' . htmlspecialchars($color) . '_rtl.css');
+	}
+}
+
+JHtml::_('bootstrap.framework');
+$doc->addScript($this->baseurl . '/templates/' . $this->template . '/javascript/md_stylechanger.js', 'text/javascript');
+$doc->addScript($this->baseurl . '/templates/' . $this->template . '/javascript/hide.js', 'text/javascript');
+$doc->addScript($this->baseurl . '/templates/' . $this->template . '/javascript/respond.src.js', 'text/javascript');
+$doc->addScript($this->baseurl . '/templates/' . $this->template . '/javascript/template.js', 'text/javascript');
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+	<head>
+		<?php require __DIR__ . '/jsstrings.php';?>
+
+      <!--<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, user-scalable=yes"/>
+		<meta name="HandheldFriendly" content="true" />
+		<meta name="apple-mobile-web-app-capable" content="YES" />-->
+
+		<jdoc:include type="head" />
+
+		<!--[if IE 7]>
+		<link href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/ie7only.css" rel="stylesheet" type="text/css" />
+		<![endif]-->
+	</head>
+	<body id="shadow">
+		<?php if ($color == 'image'):?>
+			<style type="text/css">
+				.logoheader {
+					background:url('<?php echo $this->baseurl . '/' . htmlspecialchars($headerImage); ?>') no-repeat right;
+				}
+				body {
+					background: <?php echo $templateparams->get('backgroundcolor'); ?>;
+				}
+			</style>
+		<?php endif; ?>
+
+		<div id="all">
+			<div id="back">
+				<header id="header">
+               	  <div class="container">
+                    <div class="contactt">
+  						<p class="top-contact">+7-923-508-06-66</p>                      	
+                   	</div>
+                    <div class="pozvon">                      
+                      <p class="poz-contact"><a id="show_callback" href="javascript:void(0)" title="Открыть">Позвонить Вам?</a></p>
+                      <jdoc:include type="modules" name="pozvon" />                      
+                    </div>
+                    <div class="napis">
+                      <p class="nap-contact"><a id="show_callback" href="/index.php?option=com_contact&amp;view=contact&amp;id=2">Написать нам</a></p>
+                    </div>
+					<div class="logoheader">                      
+						<h1 id="logo">                          
+						<?php if ($logo) : ?>
+							<a href="http://deeboro.ru/";><img src="<?php echo $this->baseurl; ?>/<?php echo htmlspecialchars($logo); ?>"  alt="<?php echo htmlspecialchars($templateparams->get('sitetitle')); ?>" /></a>
+						<?php endif;?>
+						<?php if (!$logo AND $templateparams->get('sitetitle')) : ?>
+							<?php echo htmlspecialchars($templateparams->get('sitetitle')); ?>
+						<?php elseif (!$logo AND $config->get('sitename')) : ?>
+							<?php echo htmlspecialchars($config->get('sitename')); ?>
+						<?php endif; ?>
+						<span class="header1">
+						<?php echo htmlspecialchars($templateparams->get('sitedescription')); ?>
+						</span></h1>                  
+					</div><!-- end logoheader -->             
+                        <!--<ul class="skiplinks">
+						<li><a href="#main" class="u2"><?php echo JText::_('TPL_BEEZ3_SKIP_TO_CONTENT'); ?></a></li>
+						<li><a href="#nav" class="u2"><?php echo JText::_('TPL_BEEZ3_JUMP_TO_NAV'); ?></a></li>-->
+						<?php if ($showRightColumn) : ?>
+							<li><a href="#right" class="u2"><?php echo JText::_('TPL_BEEZ3_JUMP_TO_INFO'); ?></a></li>
+						<?php endif; ?>
+					</ul>
+					<!--<h2 class="unseen"><?php echo JText::_('TPL_BEEZ3_NAV_VIEW_SEARCH'); ?></h2>
+					<h3 class="unseen"><?php echo JText::_('TPL_BEEZ3_NAVIGATION'); ?></h3>-->
+                  <div class="menuverh">
+                      <div class="razd">
+                        Все разделы
+                         
+                      </div>
+                  <div class="searchmenu">                 
+                 
+                   <jdoc:include type="modules" name="searchmenu" />
+                   </div>
+                  
+					<jdoc:include type="modules" name="position-1" />
+                  
+           		 </div>
+					<div id="line">
+                      	<jdoc:include type="modules" name="cart" />
+						<!--<div id="fontsize"></div>
+						<h3 class="unseen"><?php echo JText::_('TPL_BEEZ3_SEARCH'); ?></h3>
+						<jdoc:include type="modules" name="position-0" />
+					</div>--> <!-- end line -->
+                      </div>
+              
+              
+  				</header><!-- end header -->    
+              </div><!--conteiner-->
+				<div id="<?php echo $showRightColumn ? 'contentarea2' : 'contentarea'; ?>">
+					<div id="breadcrumbs">
+						<jdoc:include type="modules" name="position-2" />
+					</div>
+
+					<?php if ($navposition == 'left' and $showleft) : ?>
+						<nav class="left1 <?php if ($showRightColumn == null) { echo 'leftbigger';} ?>" id="nav">
+							<jdoc:include type="modules" name="position-7" style="beezDivision" headerLevel="3" />
+							<jdoc:include type="modules" name="position-4" style="beezHide" headerLevel="3" state="0 " />
+							<jdoc:include type="modules" name="position-5" style="beezTabs" headerLevel="2"  id="3" />
+						</nav><!-- end navi -->
+					<?php endif; ?>
+
+					<div id="<?php echo $showRightColumn ? 'wrapper' : 'wrapper2'; ?>" <?php if (isset($showno)){echo 'class="shownocolumns"';}?>>
+						<div id="main">
+
+							<?php if ($this->countModules('position-12')) : ?>
+								<div id="top">
+									<jdoc:include type="modules" name="position-12" />
+								</div>                          		
+							<?php endif; ?>
+							<div id="<?php echo $showRightColumn ? 'slide' : 'slide'; ?>">                             	
+									<jdoc:include type="modules" name="slider"/>
+							</div>
+                          	<div id="lider">
+								<jdoc:include type="modules" name="lider" style="xhtml"/>
+							</div>
+							<jdoc:include type="message" />
+							<jdoc:include type="component" />								
+						</div><!-- end main -->
+					</div><!-- end wrapper -->
+
+					<?php if ($showRightColumn) : ?>
+						<div id="close">
+							<a href="#" onclick="auf('right')">
+							<span id="bild">
+								<?php echo JText::_('TPL_BEEZ3_TEXTRIGHTCLOSE'); ?>
+							</span>
+							</a>
+						</div>
+
+						<aside id="right">
+							<h2 class="unseen"><?php echo JText::_('TPL_BEEZ3_ADDITIONAL_INFORMATION'); ?></h2>
+							<jdoc:include type="modules" name="position-6" style="beezDivision" headerLevel="3" />
+							<jdoc:include type="modules" name="position-8" style="beezDivision" headerLevel="3" />
+							<jdoc:include type="modules" name="position-3" style="beezDivision" headerLevel="3" />
+						</aside><!-- end right -->
+					<?php endif; ?>
+
+					<?php if ($navposition == 'center' and $showleft) : ?>
+						<nav class="left <?php if ($showRightColumn == null) { echo 'leftbigger'; } ?>" id="nav" >
+							<jdoc:include type="modules" name="position-7"  style="beezDivision" headerLevel="3" />
+							<jdoc:include type="modules" name="position-4" style="beezHide" headerLevel="3" state="0 " />
+							<jdoc:include type="modules" name="position-5" style="beezTabs" headerLevel="2"  id="3" />
+						</nav><!-- end navi -->
+					<?php endif; ?>
+					<div class="wrap"></div>
+				</div> <!-- end contentarea -->
+			</div><!-- back -->
+		</div><!-- all -->  
+		<div id="footer-outer">
+			<?php if ($showbottom) : ?>
+				<div id="footer-inner" >
+					<div id="bottom">
+						<div class="box box1"> <jdoc:include type="modules" name="position-9" style="beezDivision" headerlevel="3" /></div>
+						<div class="box box2"> <jdoc:include type="modules" name="position-10" style="beezDivision" headerlevel="3" /></div>
+						<div class="box box3"> <jdoc:include type="modules" name="position-11" style="beezDivision" headerlevel="3" /></div>
+					</div>
+				</div>
+			<?php endif; ?>
+			<div id="footer-sub">
+				<footer id="footer">                  
+                                <div class="containerr">
+                                 	<div class="logoniz">                                                         
+                     					<a href="http://deeboro.ru/";><img src="/images/Logo_white1.png"></a>
+                  					</div>
+                                  <div class="menuniz">
+                                    <p id="nizmenu">
+                                      <a href="">Главная</a>   |    <a href="/index.php/o-magazine">О магазине</a>   |    <a href="/index.php/dostavka-i-oplata">Доставка и оплата</a>   |    <a href="/index.php/otzyvy">Отзывы</a>
+                                    </p>
+                                  </div>
+                                  <div class="infa">
+ 									 <li class="bottom-contact"><i class="icon-map-marker"></i>«Dee Boro» 2015</li>  
+      								 <li class="bottom-contact"><i class="icon-phone"></i>+7-923-508-06-66</li>
+    								 <li class="bottom-contact"><i class="icon-envelope-alt"></i><a href="mailto:deeboro@yandex.ru">deeboro@yandex.ru</a></li>
+  									 <p></p>              
+                                  </div>
+             					 </div>	
+					<jdoc:include type="modules" name="position-14" />
+				</footer><!-- end footer -->                     
+			</div>
+		</div>
+		<jdoc:include type="modules" name="debug" />
+	</body>
+</html>
